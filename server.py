10eef5c0ba6be5e8755ea4e0979e73dcd6483a37
Pawankumar Shedage <pawankumarshedage@.gmail.com>
2025-06-13 15:26:57 +0530

    v1

--- server.py
+++ server.py
@@ -7,19 +7,19 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def root():
     # returned value will be sent to the client
-    return "hello client"
+    return "Hello From Pawan"
 
 
-@app.route('/products', methods=['GET'])
-def product_list():
-    products = [
-        {"id": 1, "title":"product1", "price": 100},
-        {"id": 2, "title":"product2", "price": 200},
-        {"id": 3, "title":"product3", "price": 300},
-        {"id": 4, "title":"product4", "price": 400}
-    ]
+#@app.route('/products', methods=['GET'])
+#def product_list():
+#   products = [
+#        {"id": 1, "title":"product1", "price": 100},
+#        {"id": 2, "title":"product2", "price": 200},
+#        {"id": 3, "title":"product3", "price": 300},
+#        {"id": 4, "title":"product4", "price": 400}
+#    ]
     # sending the list of dictionaries in JSON format to the client
-    return products
+#    return products
 
 # start the application
 app.run(host="0.0.0.0", port=4000, debug=True)
